0d8266748a822ddc9bb12212257d4710b0ba371d
Emmanuel Gabriel <degabriels03@gmail.com>
2026-05-20 16:25:44 +0530

    Added some annotations for swagger documentation

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -5,6 +5,10 @@ import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,7 +24,14 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @Tag(name = "Category APIs", description = "APIs to manage category related operations")
+    @Operation(summary = "Get all Categories", description = "Displays all the categories present.")
     @GetMapping("/public/categories")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved categories"),
+            @ApiResponse(responseCode = "404", description = "No categories found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     //@RequestMapping(value = "/public/categories", method = RequestMethod.GET)
     public ResponseEntity<CategoryResponse> getAllCategories(
             @RequestParam (name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
@@ -32,6 +43,8 @@ public class CategoryController {
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);//200 response code
     }
 
+    @Tag(name = "Category APIs", description = "APIs to manage category related operations")
+    @Operation(summary = "Create Category", description = "On hitting this API we will be able to create a new Category.")
     @PostMapping("/public/categories")
     //@RequestMapping(value = "/public/categories", method = RequestMethod.POST)
     public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO){
@@ -39,12 +52,16 @@ public class CategoryController {
         return new ResponseEntity<>(savedCategoryDTO, HttpStatus.CREATED);//201 response code
     }
 
+    @Tag(name = "Category APIs", description = "APIs to manage category related operations")
+    @Operation(summary = "Delete Category", description = "On hitting this API we will be able to delete an existing Category with a specific categoryId.")
     @DeleteMapping("admin/categories/{categoryId}")
     public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long categoryId){
         CategoryDTO deletedCategory = categoryService.deleteCategory(categoryId);
         return new ResponseEntity<>(deletedCategory, HttpStatus.OK);
     }
 
+    @Tag(name = "Category APIs", description = "APIs to manage category related operations")
+    @Operation(summary = "Update Category", description = "On hitting this API we will be able to update an existing Category with a specific categoryId.")
     @PutMapping("/admin/categories/{categoryId}")
     public ResponseEntity<CategoryDTO> updateCategory(@Valid @RequestBody CategoryDTO categoryDTO, @PathVariable Long categoryId){
         CategoryDTO savedCategoryDTO = categoryService.updateCategory(categoryDTO, categoryId);
