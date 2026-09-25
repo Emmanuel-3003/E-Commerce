f658ee4960bd8d2decfe8aeee8d1a619ec0f023f
Emmanuel Gabriel <degabriels03@gmail.com>
2026-09-25 13:18:52 +0530

    product rendering and cors issue handling

---
+++ ecom-frontend/src/store/actions/index.js
@@ -0,0 +1,19 @@
+import api from "../../api/api";
+
+export const fetchProducts = () => async (dispatch) => {
+    try {
+        const {data} = await api.get(`/public/products`);
+        dispatch({
+            type: "FETCH_PRODUCTS",
+            payload: data.content,
+            pageNumber: data.pageNumber,
+            pageSize: data.pageSize,
+            totalElement: data.totalElement,
+            totalPages: data.totalPages,
+            lastPage: data.lastPage,
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
---
+++ ecom-frontend/src/store/reducers/ProductReducer.js
@@ -0,0 +1,26 @@
+const initialState = {
+    products: null,
+    categories: null,
+    pagination: {},
+}
+
+export const productReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "FETCH_PRODUCTS":
+            return{
+                ...state,
+                products: action.payload,
+                pagination: {
+                    ...state.pagination,
+                    pageNumber: action.pageNumber,
+                    pageSize: action.pageSize,
+                    totalElement: action.totalElement,
+                    totalPages: action.totalPages,
+                    lastPage: action.lastPage,
+                },
+            };
+    
+        default:
+            return state;
+    }
+};
